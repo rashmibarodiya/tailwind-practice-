20225c15125e555a1696938a7e7692e3840cd06e
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-08-14 17:04:08 +0000

    creating appbar

---
+++ tailapp/src/app/appbar/page.tsx
@@ -0,0 +1,19 @@
+
+
+export default function Appbar(){
+    return(
+        <>
+        <div className="flex">
+            <div className="flex-none w-64">
+                youtube
+            </div>
+            <div className="flex-auto w-64">
+                search
+            </div>
+            <div className="flex-auto w-64">
+                profile
+            </div>
+        </div>
+        </>
+    )
+}
